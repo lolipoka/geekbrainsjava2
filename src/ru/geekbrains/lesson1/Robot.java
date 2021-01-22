cc80e8ab1396cb4c570621781b42b2ffd01abb91
Alexey Adamov <booqueworme@gmail.com>
2021-01-22 17:19:50 +0300

    Lesson 1 home work

---
+++ src/ru/geekbrains/lesson1/Robot.java
@@ -0,0 +1,44 @@
+package ru.geekbrains.lesson1;
+
+public class Robot implements Competable {
+    private final String name;
+    private final double maxDistance;
+    private final double maxHeight;
+    private boolean outOfCompetition;
+
+    public Robot(String name, double maxDistance, double maxHeight) {
+        this.name = name;
+        this.maxDistance = maxDistance;
+        this.maxHeight = maxHeight;
+        outOfCompetition = false;
+    }
+
+    @Override
+    public boolean isOutOfCompetition() {
+        return outOfCompetition;
+    }
+
+    @Override
+    public boolean jump(double height) {
+        if (height > maxHeight) {
+            System.out.printf("%s failed to jump over %.2f meters.\n", name, height);
+            outOfCompetition = true;
+            return false;
+        } else {
+            System.out.printf("%s successfully jumped over %.2f meters.\n", name, height);
+            return true;
+        }
+    }
+
+    @Override
+    public boolean run(double distance) {
+        if (distance > maxDistance) {
+            System.out.printf("%s failed to run %.2f meters.\n", name, distance);
+            outOfCompetition = true;
+            return false;
+        } else {
+            System.out.printf("%s successfully ran %.2f meters.\n", name, distance);
+            return true;
+        }
+    }
+}
